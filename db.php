7d45c00ab2b1cf9e924a3fdfaf1162fabe124fe7
sulthandk <69774546+sulthandk@users.noreply.github.com>
2024-12-04 09:29:40 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,18 @@
 <?php
+/*
+ * Hak Cipta © 2024 Muhammad Sulthan Dary Kurahman
+ *
+ * Tidak diperkenankan menyalin, mendistribusikan ulang, atau memodifikasi kode ini tanpa izin tertulis
+ * dari pemilik hak cipta.
+ *
+ * Ketentuan Penggunaan:
+ * 1. Hanya untuk keperluan pendidikan atau non-komersial, dengan mencantumkan kredit kepada pemilik.
+ * 2. Dilarang mengklaim sebagian atau seluruh kode ini sebagai milik Anda.
+ *
+ * Hubungi: msulthandaryk@gmail.com jika Anda memerlukan izin atau menemukan pelanggaran.
+ *
+ * Terima kasih telah menghormati karya saya!
+ */
 // Definisikan detail koneksi database hanya jika belum didefinisikan
 if (!defined("HOSTNAME")) {
     define("HOSTNAME", "localhost");
