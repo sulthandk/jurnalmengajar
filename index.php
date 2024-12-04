7eca4b814b5bf4fb50cd86a38094dfcb3128ba38
sulthandk <69774546+sulthandk@users.noreply.github.com>
2024-12-04 09:30:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+/*
+ * Hak Cipta © 2024 Muhammad Sulthan Dary Kurahman
+ *
+ * Tidak diperkenankan menyalin, mendistribusikan ulang, atau memodifikasi kode ini tanpa izin tertulis
+ * dari pemilik hak cipta.
+ *
+ * Ketentuan Penggunaan:
+ * 1. Hanya untuk keperluan pendidikan atau non-komersial, dengan mencantumkan kredit kepada pemilik.
+ * 2. Dilarang mengklaim sebagian atau seluruh kode ini sebagai milik Anda.
+ *
+ * Hubungi: msulthandaryk@gmail.com jika Anda memerlukan izin atau menemukan pelanggaran.
+ *
+ * Terima kasih telah menghormati karya saya!
+ */
 session_start();
 include('db.php'); // File koneksi database
 
